5c7f73734b1fa39a5bcf03dd1fc687b0fb829c81
Hossein Mohammadi <hosseinm.developer@gmail.com>
2020-12-16 13:00:22 +0330

    perf: add beforeunload to handle close tabs

--- src/context.ts
+++ src/context.ts
@@ -8,6 +8,9 @@ import {
 import hermes from "hermes-channel";
 import { DependencyList, useEffect } from "react";
 
+// @ts-ignore
+const __DEV__ = process.env.NODE_ENV !== "production";
+
 const IS_SIGNAL_R_CONNECTED = "IS_SIGNAL_R_CONNECTED";
 
 export interface ProviderProps extends IHttpConnectionOptions {
@@ -104,27 +107,37 @@ function providerFactory<T extends string>(
         return;
       }
 
-      const connection = new HubConnectionBuilder()
-        .withUrl(url, {
-          ...rest,
-        })
-        .withAutomaticReconnect()
-        .configureLogging(LogLevel.Information)
-        .build();
+      let connectionBuilder = new HubConnectionBuilder()
+        .withUrl(url, rest)
+        .withAutomaticReconnect();
+
+      if (__DEV__) {
+        connectionBuilder = connectionBuilder.configureLogging(
+          LogLevel.Information,
+        );
+      }
+
+      const connection = connectionBuilder.build();
 
       Context.connection = connection;
-      let lastConnectionSentState: number;
+      let lastConnectionSentState: number | null = null;
       let connectionId: string | null = null;
 
       /** If another tab connected to signalR we will receive this event */
       hermes.on(IS_SIGNAL_R_CONNECTED, (_connectionId) => {
+        // connected tab will send empty _connectionId before close
+        if (!_connectionId) {
+          lastConnectionSentState = null;
+          connectionId = null;
+          checkForStart();
+
+          return;
+        }
+
         console.log("_connectionId");
         connectionId = _connectionId;
         lastConnectionSentState = Date.now();
-        if (
-          connection.state === HubConnectionState.Disconnected ||
-          connection.state === HubConnectionState.Disconnecting
-        ) {
+        if (!isConnectionConnecting(connection)) {
           sentInterval && clearInterval(sentInterval);
           connection.stop();
         }
@@ -144,44 +157,39 @@ function providerFactory<T extends string>(
 
       let sentInterval: any;
 
-      async function start() {
-        try {
-          if (connectionId) {
-            return;
-          }
-
-          hermes.send(IS_SIGNAL_R_CONNECTED, connection.connectionId);
-          await connection.start();
-          sentInterval = setInterval(() => {
-            if (connectionId) {
-              clearInterval(sentInterval);
-              connection.stop();
-              return;
-            }
-            hermes.send(IS_SIGNAL_R_CONNECTED, connection.connectionId);
-          }, 4000);
-        } catch (err) {
-          console.log(err);
-          clearInterval(sentInterval);
-          if (connectionId) {
-            return;
-          }
-        }
-      }
-
-      const checkForStart = () => {
+      async function checkForStart() {
         checkExpectedSignalRMessages();
 
         if (
           (!lastConnectionSentState ||
             lastConnectionSentState < Date.now() - 5000) &&
-          (connection.state === HubConnectionState.Disconnected ||
-            connection.state === HubConnectionState.Disconnecting)
+          !isConnectionConnecting(connection)
         ) {
-          connectionId = null;
-          start();
+          try {
+            hermes.send(IS_SIGNAL_R_CONNECTED, connection.connectionId);
+            await connection.start();
+
+            function syncWithTabs() {
+              if (connectionId) {
+                clearInterval(sentInterval);
+                connection.stop();
+                return;
+              }
+              hermes.send(IS_SIGNAL_R_CONNECTED, connection.connectionId);
+            }
+
+            syncWithTabs();
+
+            sentInterval = setInterval(syncWithTabs, 4000);
+          } catch (err) {
+            console.log(err);
+            clearInterval(sentInterval);
+            if (connectionId) {
+              return;
+            }
+          }
         }
-      };
+      }
 
       checkForStart();
 
@@ -192,6 +200,20 @@ function providerFactory<T extends string>(
           hermes.send(event, message, true);
         });
       });
+
+      /** Before of this tab close this event will sent an empty connectionId to other tabs */
+      function onBeforeunload() {
+        if (isConnectionConnecting(connection)) {
+          hermes.send(IS_SIGNAL_R_CONNECTED, "");
+
+          clearInterval(sentInterval);
+          connection.stop();
+          return;
+        }
+      }
+
+      /** AddEventListener is not exist in react-native */
+      window?.addEventListener?.("beforeunload", onBeforeunload);
 
       return () => {
         clearInterval(checkInterval);
@@ -206,6 +228,9 @@ function providerFactory<T extends string>(
             expectedCallbacks[index],
           );
         });
+
+        /** RemoveEventListener is not exist in react-native */
+        window?.removeEventListener?.("beforeunload", onBeforeunload);
       };
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [connectEnabled, ...dependencies]);
@@ -214,6 +239,14 @@ function providerFactory<T extends string>(
   };
 
   return Provider;
+}
+
+function isConnectionConnecting(connection: HubConnection) {
+  return (
+    connection.state === HubConnectionState.Connected ||
+    connection.state === HubConnectionState.Reconnecting ||
+    connection.state === HubConnectionState.Connecting
+  );
 }
 
 export { createSignalRContext };
