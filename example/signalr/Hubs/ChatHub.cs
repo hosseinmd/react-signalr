93f804415e41e1900adbca972e355dfd3e4e13ea
Hossein Mohammadi <hosseinm.developer@gmail.com>
2023-09-06 16:47:11 +0330

    chore: fix signalr socket

--- example/signalr/Hubs/ChatHub.cs
+++ example/signalr/Hubs/ChatHub.cs
@@ -1,11 +1,33 @@
 using Example.Hubs.Clients;
 using Example.ViewModel;
+using System;
 using System.Threading.Tasks;
 
 namespace Example.Hubs
 {
     public class ChatHub : AppHubBase<IChatClient> , IHuB
     {
+         public ChatHub()
+        {
+            // this.chatHub = chatHub;
+
+           // Par  t 1: setup the timer for 3 seconds.
+            var timer = new System.Timers.Timer(1000);
+            // To add the elapsed event handler:
+            // ... Type "_timer.Elapsed += " and press tab twice.
+            void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+            {
+                    Clients.All.Hello();
+            }
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(_timer_Elapsed);
+            timer.Enabled = true;
+        }
+        public async Task Hello()
+        {
+            await Clients.All.Hello();
+        }
+
         public async Task<string> StartWorkAsync(StartWorkVm message)
         {
             await Clients.All.StartWorkAsync(message);
